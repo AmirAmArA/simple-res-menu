5eec829b4770bef50ee372713d0f4b04c7998a3c
Amir Amara <amiramara11@gmail.com>
2024-06-11 17:31:49 +0300

    move to dynamic data

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 
-export default function MenuItem() {
+interface MenuItemProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ title, description, imageUrl }) => {
   return (
-    <div className='flex justify-center items-center'>
-
+    <div className="flex justify-center items-center">
       <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <figure>
+          <img src={imageUrl} alt={title} />
+        </figure>
         <div className="px-3 py-2 h-20">
-          <h2 className="text-base font-black">Shoes!</h2>
-          <p className='text-xs'>If a dog chews shoes whose shoes does he choose?</p>
-
+          <h2 className="text-base font-black">{title}</h2>
+          <p className="text-xs">{description}</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default MenuItem;
